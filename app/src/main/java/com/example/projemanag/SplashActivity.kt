084ca23b019a29c6e3a5ccc39383cb3db3e0277a
Danish <iamdanishk555@gmail.com>
2022-05-15 22:46:37 +0545

    initial commit

---
+++ app/src/main/java/com/example/projemanag/SplashActivity.kt
@@ -0,0 +1,17 @@
+package com.example.projemanag
+
+import android.graphics.Typeface
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowManager
+import android.widget.TextView
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+
+
+    }
+}
